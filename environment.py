a1401fea49501a78c5ff92433460eec174aa8e54
cuong111103hd <cuong111103hd@gmail.com>
2024-07-07 16:01:37 +0700

    First test

--- environment.py
+++ environment.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 class RIS_MISO(object):
     def __init__(self,
                  num_antennas,
@@ -13,10 +12,12 @@ class RIS_MISO(object):
         self.M = num_antennas
         self.L = num_RIS_elements
         self.K = num_users
-        self.L_t = self.L/2
-        self.L_r = self.L/2
-        self.K_t = self.K/2
-        self.K_r = self.K/2
+
+        # Ensure integer division
+        self.L_t = int(self.L // 2)
+        self.L_r = int(self.L // 2)
+        self.K_t = int(self.K // 2)
+        self.K_r = int(self.K // 2)
 
         self.channel_est_error = channel_est_error
 
@@ -26,7 +27,6 @@ class RIS_MISO(object):
         self.channel_noise_var = channel_noise_var
 
         power_size = 2 * self.K
-
         channel_size = 2 * (self.L * self.M + self.L * self.K)
 
         self.action_dim = 2 * self.M * self.K + 2 * self.L
@@ -50,11 +50,10 @@ class RIS_MISO(object):
     def reset(self):
         self.episode_t = 0
 
-        self.H_1 = np.random.normal(0, np.sqrt(0.5), (self.L, self.M)) + 1j * np.random.normal(0, np.sqrt(0.5),
-                                                                                               (self.L, self.M))
+        self.H_1 = np.random.normal(0, np.sqrt(0.5), (self.L, self.M)) + 1j * np.random.normal(0, np.sqrt(0.5), (self.L, self.M))
 
-        self.h_t = np.random.normal(0, np.sqrt(0.5), (self.L, int(self.K_r))) + np.random.normal(0, np.sqrt(0.5), (self.L, int(self.K_r))) * 1j
-        self.h_r = np.random.normal(0, np.sqrt(0.5), (self.L, int(self.K_t))) + np.random.normal(0, np.sqrt(0.5), (self.L, int(self.K_t))) * 1j
+        self.h_t = np.random.normal(0, np.sqrt(0.5), (self.L, self.K_r)) + np.random.normal(0, np.sqrt(0.5), (self.L, self.K_r)) * 1j
+        self.h_r = np.random.normal(0, np.sqrt(0.5), (self.L, self.K_t)) + np.random.normal(0, np.sqrt(0.5), (self.L, self.K_t)) * 1j
 
         init_action_G = np.hstack((np.real(self.G.reshape(1, -1)), np.imag(self.G.reshape(1, -1))))
         init_action_Phi = np.hstack((np.real(np.diag(self.Phi)).reshape(1, -1), np.imag(np.diag(self.Phi)).reshape(1, -1)))
@@ -68,13 +67,11 @@ class RIS_MISO(object):
 
         power_t = np.real(np.diag(self.G.conjugate().T @ self.G)).reshape(1, -1) ** 2
 
-        # H_2_tilde = self._compute_H_2_tilde()
         h_t_tilde = self._compute_tilde(self.h_t)
         h_r_tilde = self._compute_tilde(self.h_r)
         power_r = np.linalg.norm(h_t_tilde, axis=0).reshape(1, -1) ** 2 + np.linalg.norm(h_r_tilde, axis=0).reshape(1, -1) ** 2
 
         H_1_real, H_1_imag = np.real(self.H_1).reshape(1, -1), np.imag(self.H_1).reshape(1, -1)
-        # H_2_real, H_2_imag = np.real(self.H_2).reshape(1, -1), np.imag(self.H_2).reshape(1, -1)
         h_t_real, h_t_img = np.real(self.h_t).reshape(1, -1), np.imag(self.h_t).reshape(1, -1)
         h_r_real, h_r_img = np.real(self.h_r).reshape(1, -1), np.imag(self.h_r).reshape(1, -1)
 
@@ -85,8 +82,8 @@ class RIS_MISO(object):
     def _compute_reward(self, Phi):
 
         diag_Phi = np.diag(Phi)
-        diag_Phi1 = np.zeros((self.L, 1))
-        diag_Phi2 = np.zeros((self.L, 1))
+        diag_Phi1 = np.zeros((self.L,), dtype=complex)
+        diag_Phi2 = np.zeros((self.L,), dtype=complex)
         diag_Phi1[:self.L_t] = diag_Phi[:self.L_t]
         diag_Phi2[self.L_t:] = diag_Phi[self.L_t:]
 
@@ -96,23 +93,23 @@ class RIS_MISO(object):
         opt_reward = 0
 
         for k in range(self.K):
-            if ( k < self.K_t):
+            if k < self.K_t:
                 h = self.h_t
-                Phi = Phi1
+                Phi_k = Phi1
+                h_k = h[:, k].reshape(-1, 1)
             else:
                 h = self.h_r
-                Phi = Phi2
+                Phi_k = Phi2
+                h_k = h[:, k-self.K_t].reshape(-1, 1)
 
-            h_k = h[:, k].reshape(-1, 1)
+
             g_k = self.G[:, k].reshape(-1, 1)
 
-            x = np.abs(h_k.T @ Phi @ self.H_1 @ g_k) ** 2
-
+            x = np.abs(h_k.T @ Phi_k @ self.H_1 @ g_k) ** 2
             x = x.item()
 
             G_removed = np.delete(self.G, k, axis=1)
-
-            interference = np.sum(np.abs(h_k.T @ Phi @ self.H_1 @ G_removed) ** 2)
+            interference = np.sum(np.abs(h_k.T @ Phi_k @ self.H_1 @ G_removed) ** 2)
             y = interference + (self.K - 1) * self.awgn_var
 
             rho_k = x / y
@@ -134,12 +131,10 @@ class RIS_MISO(object):
         Phi_imag = action[:, -self.L:]
 
         self.G = G_real.reshape(self.M, self.K) + 1j * G_imag.reshape(self.M, self.K)
-
         self.Phi = np.eye(self.L, dtype=complex) * (Phi_real + 1j * Phi_imag)
 
         power_t = np.real(np.diag(self.G.conjugate().T @ self.G)).reshape(1, -1) ** 2
 
-        # H_2_tilde = self._compute_H_2_tilde()
         h_t_tilde = self._compute_tilde(self.h_t)
         h_r_tilde = self._compute_tilde(self.h_r)
 
